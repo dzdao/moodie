5607f7fb20a66c469efcb9cadb3545c7dc58c6d2
Rodrigo monroy <rui@Rodrigos-MacBook-Pro.local>
2016-07-15 03:44:26 -0700

    yelp's API shoueld be ready to use

--- location/app/src/main/java/com/example/rui/location/MainActivity.java
+++ location/app/src/main/java/com/example/rui/location/MainActivity.java
@@ -9,16 +9,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.Business;
+import com.yelp.clientlib.entities.SearchResponse;
+import com.yelp.clientlib.entities.options.BoundingBoxOptions;
+import com.yelp.clientlib.entities.options.CoordinateOptions;
+
 import java.security.Provider;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
 {
+
  String location = "you're in: ";
     double lat;
     double lon;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        YelpAPIFactory apiFactory = new YelpAPIFactory("GH0hCC83JR1G-T_7T54jxw", "Dw-cj6EtFAIRpu9pzSRjEuHEUNs", "LFzLyFa_z8Id4q9TvwfFgE-StpfbU4LN", "5lNteBpwcpLj8vBCJprPsnXEbL8");
+        YelpAPI yelpAPI = apiFactory.createAPI();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getCoordinates();
@@ -30,6 +41,7 @@ public class MainActivity extends AppCompatActivity
         TextView text= (TextView) findViewById(R.id.textLocation);
         String currentLocation = location + "Latitude: "+ lat +" Longitude: " + lon;
         text.setText ( currentLocation );
+
     }
 
     private void getCoordinates()
