690c8b0228d43943a3885fa8ad2804e69c89ab57
Rodrigo monroy <rui@Rodrigos-MacBook-Pro.local>
2016-07-21 23:18:27 -0700

    updated code with Restaurant class

---
+++ moodie/moodie/src/main/java/com/example/rui/location/mood.java
@@ -0,0 +1,31 @@
+package com.example.rui.location;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Call;
+
+/**
+ * Created by rui on 7/21/16.
+ */
+public class mood
+{
+    String name;
+    String imageURL;
+    String apiKey = "dc6zaTOxFJmzC";
+    //name is the mood it self i.e. happy, sad.
+    String giphyUrl = "http://api.giphy.com/v1/gifs/random" +
+            "?api_key=" +
+            apiKey +
+            "&tag=" +
+            "cat";
+    public mood(String name)
+    {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(giphyUrl)
+                .build();
+    }
+
+
+}
