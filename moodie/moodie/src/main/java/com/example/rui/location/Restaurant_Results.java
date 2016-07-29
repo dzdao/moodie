7e1b3c87951454ebc7f85061e4f523bd41956d67
rui figueroa <ruimonroy@csu.fullerton.edu>
2016-07-29 15:30:01 -0700

    latest version of my code

--- moodie/moodie/src/main/java/com/example/rui/location/Restaurant_Results.java
+++ moodie/moodie/src/main/java/com/example/rui/location/Restaurant_Results.java
@@ -75,7 +75,7 @@ public class Restaurant_Results extends AppCompatActivity
     String numberOfResults = "8"; //limit the number of results to 10 businesses
     String category_filter = "food";
 
-    Restaurant restaurant;
+    Restaurant[] restaurant;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -88,6 +88,7 @@ public class Restaurant_Results extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        restaurant = new Restaurant[8];
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant__results);
@@ -103,6 +104,8 @@ public class Restaurant_Results extends AppCompatActivity
         String con_test = "";
 
          yelp();
+
+
     }
 
 
@@ -122,13 +125,12 @@ public class Restaurant_Results extends AppCompatActivity
         lat = loc.getLatitude();
     }
 
-    private void yelp()
-    {
-        String consumerKey ="GH0hCC83JR1G-T_7T54jxw";
-        String consumerSecret="Dw-cj6EtFAIRpu9pzSRjEuHEUNs";
-        String token="VPvzcqEfLh07yrHaAzIARouynBWnDjxv";
-        String tokenSecret="QYcm0Coq4XRfjLTSfyCv4Zlb38c";
-        YelpAPIFactory apiFactory = new YelpAPIFactory(consumerKey,consumerSecret , token,tokenSecret );
+    private void yelp() {
+        String consumerKey = "GH0hCC83JR1G-T_7T54jxw";
+        String consumerSecret = "Dw-cj6EtFAIRpu9pzSRjEuHEUNs";
+        String token = "VPvzcqEfLh07yrHaAzIARouynBWnDjxv";
+        String tokenSecret = "QYcm0Coq4XRfjLTSfyCv4Zlb38c";
+        YelpAPIFactory apiFactory = new YelpAPIFactory(consumerKey, consumerSecret, token, tokenSecret);
         YelpAPI yelpAPI = apiFactory.createAPI();
 
         Map<String, String> params = new HashMap<>();
@@ -146,15 +148,14 @@ public class Restaurant_Results extends AppCompatActivity
         Call<SearchResponse> call = yelpAPI.search(coordinate, params);
 
         // setup for asynchronous request
-        Callback<SearchResponse> callback = new Callback<SearchResponse>()
-        {
+        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
 
             //TextView con = (TextView) findViewById(R.id.resultsView);
             String con_test = "";
 
             @Override
-            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response)
-            {   ImageView img = (ImageView) findViewById(R.id.image);
+            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+                ImageView img = (ImageView) findViewById(R.id.image);
                 ImageView img2 = (ImageView) findViewById(R.id.image2);
                 ImageView img3 = (ImageView) findViewById(R.id.image3);
                 ImageView img4 = (ImageView) findViewById(R.id.image4);
@@ -168,17 +169,17 @@ public class Restaurant_Results extends AppCompatActivity
 
                 SearchResponse searchResponse = response.body();
                 final ArrayList<Business> businesses = searchResponse.businesses();
-                //TextView text = (TextView) findViewById(R.id.resultView);
+                // TextView text = (TextView) findViewById(R.id.resultView);
+                //String businessString= businesses.get(0).toString();
 
+                //text.setText(businessString);
 
                 //pass results to next activity
-                final String businessName =  businesses.get(0).name();
+                final String businessName = businesses.get(0).name();
                 String businessAddress;
-                try
-                {
+                try {
                     businessAddress = businesses.get(0).location().address().get(0);
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     businessAddress = "no address available";
                 }
 
@@ -187,40 +188,52 @@ public class Restaurant_Results extends AppCompatActivity
                 final String city = businesses.get(0).location().city();
                 final String state = businesses.get(0).location().stateCode();
                 final String reviewSnippet = businesses.get(0).snippetText();
-                final String imageURL =businesses.get(0).imageUrl();
+                final String imageURL = businesses.get(0).imageUrl();
                 // convert meters to miles
                 distance = distance / 1609.34;
 
 
-               // text.setText(res);
+                // text.setText(res);
                 for (int i = 0; i < Integer.parseInt(numberOfResults); i++)
                 {
-                    Picasso.with(getApplicationContext()).load(businesses.get(0).imageUrl()).into(img);
-                    Picasso.with(getApplicationContext()).load(businesses.get(1).imageUrl()).into(img2);
-                    Picasso.with(getApplicationContext()).load(businesses.get(2).imageUrl()).into(img3);
-                    Picasso.with(getApplicationContext()).load(businesses.get(3).imageUrl()).into(img4);
-                    Picasso.with(getApplicationContext()).load(businesses.get(4).imageUrl()).into(img5);
-                    Picasso.with(getApplicationContext()).load(businesses.get(5).imageUrl()).into(img6);
-                    Picasso.with(getApplicationContext()).load(businesses.get(6).imageUrl()).into(img7);
-                    Picasso.with(getApplicationContext()).load(businesses.get(7).imageUrl()).into(img8);
+                    String address;
+                    try {
+                        address =businesses.get(i).location().address().get(i) ;
+                    }
+                    catch (Exception e){
+                        address = "no address available";
+                    }
+                    restaurant[i] = new Restaurant(businesses.get(i).name(), businesses.get(i).phone(),
+                            address, businesses.get(i).distance(), businesses.get(i).snippetText(),
+                            businesses.get(i).imageUrl(),businesses.get(i).location().city(),businesses.get(i).location().stateCode());
                 }
+                Picasso.with(getApplicationContext()).load(businesses.get(0).imageUrl()).resize(250, 250).centerInside().into(img);
+                Picasso.with(getApplicationContext()).load(restaurant[1].getImageURL()).resize(250,250).centerInside().into(img2);
+                //Picasso.with(getApplicationContext()).load(businesses.get(2).imageUrl()).resize(250,250).centerInside().into(img3);
+                //Picasso.with(getApplicationContext()).load(businesses.get(3).imageUrl()).resize(250,250).centerInside().into(img4);
+                //Picasso.with(getApplicationContext()).load(businesses.get(4).imageUrl()).resize(250,250).centerInside().into(img5);
+                //Picasso.with(getApplicationContext()).load(businesses.get(5).imageUrl()).resize(250,250).centerInside().into(img6);
+                //Picasso.with(getApplicationContext()).load(businesses.get(6).imageUrl()).resize(250,250).centerInside().into(img7);
+                //Picasso.with(getApplicationContext()).load(businesses.get(7).imageUrl()).resize(250,250).centerInside().into(img8);
                 final String finalBusinessAddress = businessAddress;
                 final double finalDistance = distance;
-                img.setOnTouchListener(new View.OnTouchListener()
-                {
+                TextView text = (TextView) findViewById(R.id.test);
+                String display = restaurant[0].getName() + "   " + restaurant[0].getPhoneNumber();
+                text.setText(display);
+                img.setOnTouchListener(new View.OnTouchListener() {
                     @Override
-                    public boolean onTouch(View v, MotionEvent event)
-                    {Intent toRestaurantDetails = new Intent(Restaurant_Results.this, RestaurantDetails.class);
+                    public boolean onTouch(View v, MotionEvent event) {
+                        Intent toRestaurantDetails = new Intent(Restaurant_Results.this, RestaurantDetails.class);
 
-                        toRestaurantDetails.putExtra("name",businessName);
+                        toRestaurantDetails.putExtra("name", businessName);
                         toRestaurantDetails.putExtra("address", finalBusinessAddress);
-                        toRestaurantDetails.putExtra("phoneNumber",businessPhoneNumber);
+                        toRestaurantDetails.putExtra("phoneNumber", businessPhoneNumber);
                         toRestaurantDetails.putExtra("imageURL", imageURL);
-                        toRestaurantDetails.putExtra("city",city);
-                        toRestaurantDetails.putExtra("state",state);
-                        toRestaurantDetails.putExtra("reviewSnippet",reviewSnippet);
-                        toRestaurantDetails.putExtra("distance",finalDistance);
-                        toRestaurantDetails.putExtra("term",term);
+                        toRestaurantDetails.putExtra("city", city);
+                        toRestaurantDetails.putExtra("state", state);
+                        toRestaurantDetails.putExtra("reviewSnippet", reviewSnippet);
+                        toRestaurantDetails.putExtra("distance", finalDistance);
+                        toRestaurantDetails.putExtra("term", term);
                         startActivity(toRestaurantDetails);
                         return false;
                     }
@@ -229,16 +242,20 @@ public class Restaurant_Results extends AppCompatActivity
             }
 
             @Override
-            public void onFailure (Call<SearchResponse> call, Throwable t)
-            {
+            public void onFailure(Call<SearchResponse> call, Throwable t) {
                 // HTTP error happened, do something to handle it.
+                TextView connectionTest = (TextView) findViewById(R.id.test);
                 con_test = "No conection";
+                connectionTest.setText(con_test);
             }
 
         };
 
+
+
+
         // make the asynchronous request
-        call.enqueue(callback);
+           call.enqueue(callback);
     }
     @Override
     public void onBackPressed()
