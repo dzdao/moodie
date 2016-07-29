396bdb71a43ef2bed26cae64f8ead76bea62cb82
DGalvan23 <dianag118@csu.fullerton.edu>
2016-07-28 22:43:43 -0700

    Update RestaurantDetails.java
    some formatting but not done

--- moodie/moodie/src/main/java/com/example/rui/location/RestaurantDetails.java
+++ moodie/moodie/src/main/java/com/example/rui/location/RestaurantDetails.java
@@ -1,43 +1,47 @@
 package com.example.rui.location;
 
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.squareup.picasso.Picasso;
-import com.yelp.clientlib.connection.YelpAPI;
-import com.yelp.clientlib.connection.YelpAPIFactory;
-import com.yelp.clientlib.entities.Business;
-import com.yelp.clientlib.entities.SearchResponse;
-import com.yelp.clientlib.entities.options.CoordinateOptions;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class RestaurantDetails extends AppCompatActivity {
-    String details;
+    String name;
+    String address;
+    String phoneNumber;
+    double distance;
+    String city;
+    String imageURL;
+    String reviewSnippet;
+    String state;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_details);
         //get data from previous activity
         Bundle getTerm = getIntent().getExtras();
-        this.details=getTerm.getString("results");
+        this.name=getTerm.getString("name");
+        this.address=getTerm.getString("address");
+        this.phoneNumber=getTerm.getString("phoneNumber");
+        this.distance=getTerm.getDouble("distance");
+        this.state=getTerm.getString("state");
+        this.city=getTerm.getString("city");
+        this.reviewSnippet=getTerm.getString("reviewSnippet");
+        this.imageURL=getTerm.getString("imageURL");
+
+        String RestaurantData="Restaurant Name: " + name +"\n\n" +
+                "Phone Number: " +
+                this.phoneNumber+"\n\n"+
+                "Address: " +
+                this.address +'\n'+
+                "                 " +
+                this.city+", "+this.state+"\n\n" +
+                "Review: " +
+                this.reviewSnippet+'\n'+'\n'+
+                this.imageURL;
 
         TextView dets = (TextView) findViewById(R.id.details);
-        dets.setText(details);
+        dets.setText(RestaurantData);
 
     }
 
