11ba4c50fd581c1d226285d1e52ad89aabc799f5
saralip <saralip@users.noreply.github.com>
2016-07-30 13:22:36 -0700

    Add files via upload

---
+++ RestaurantDetails.java
@@ -0,0 +1,74 @@
+package com.example.rui.location;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class RestaurantDetails extends AppCompatActivity {
+    String name;
+    String address;
+    String phoneNumber;
+    double distance;
+    String city;
+    String imageURL;
+    String reviewSnippet;
+    String state;
+
+    String term;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_details);
+
+        //get data from previous activity
+        Bundle getTerm = getIntent().getExtras();
+
+        this.name=getTerm.getString("name");
+        this.imageURL=getTerm.getString("imageURL");
+        this.address=getTerm.getString("address");
+        this.phoneNumber=getTerm.getString("phoneNumber");
+        this.distance=getTerm.getDouble("distance");
+        this.state=getTerm.getString("state");
+        this.city=getTerm.getString("city");
+        this.reviewSnippet=getTerm.getString("reviewSnippet");
+
+        String RestaurantDataName= name;
+        String RestaurantData2= "Phone Number: " +
+                this.phoneNumber+"\n\n"+
+                "Address: " +
+                this.address +'\n'+
+                "                 " +
+                this.city+", "+this.state+"\n\n" +
+                "Review: " +
+                this.reviewSnippet+ "\n\n";
+        
+        ImageView img = (ImageView) findViewById(R.id.image);
+
+        Picasso.with(getApplicationContext()).load(imageURL).into(img);
+
+
+        TextView dets = (TextView) findViewById(R.id.details);
+        dets.setText(RestaurantDataName);
+
+        TextView dets2 = (TextView) findViewById(R.id.details2);
+        dets2.setText(RestaurantData2);
+
+        this.term=getTerm.getString("term");
+
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        Intent intent = new Intent(this, Restaurant_Results.class);
+        intent.putExtra("term",term);
+        startActivity(intent);
+    }
+
+}
---
+++ Restaurant_Results.java
@@ -0,0 +1,266 @@
+package com.example.rui.location;
+
+//import android.content.Context;
+//import android.location.Criteria;
+//import android.location.Location;
+//import android.location.LocationListener;
+//import android.location.LocationManager;
+//import android.net.Uri;
+
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//import android.widget.TextView;
+//
+//import com.google.android.gms.appindexing.Action;
+//import com.google.android.gms.appindexing.AppIndex;
+//import com.google.android.gms.common.api.GoogleApiClient;
+//import com.yelp.clientlib.connection.YelpAPI;
+//import com.yelp.clientlib.connection.YelpAPIFactory;
+//import com.yelp.clientlib.entities.Business;
+//import com.yelp.clientlib.entities.SearchResponse;
+//import com.yelp.clientlib.entities.options.BoundingBoxOptions;
+//import com.yelp.clientlib.entities.options.CoordinateOptions;
+//
+//import java.security.Provider;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//import retrofit2.Response;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.squareup.picasso.Picasso;
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.Business;
+import com.yelp.clientlib.entities.SearchResponse;
+import com.yelp.clientlib.entities.options.CoordinateOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Restaurant_Results extends AppCompatActivity
+{
+    String location = "you're @ ";
+    double lat = 33.8823;
+    double lon = -117.883;
+
+    // default parameters
+    String term = ""; //used to always look for food places
+    String numberOfResults = "8"; //limit the number of results to 10 businesses
+    String category_filter = "food";
+
+    Restaurant[] restaurant;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    int search_category;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        restaurant = new Restaurant[8];
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant__results);
+
+        //get data from previous activity
+        Bundle getTerm = getIntent().getExtras();
+        this.term=getTerm.getString("term");
+
+        Intent toRestaurantResults = new Intent(Restaurant_Results.this, RestaurantDetails.class);
+        //toRestaurantResults.putExtra("response", yelpsearchResponse);
+        //getCoordinates();
+
+        String con_test = "";
+
+         yelp();
+
+
+    }
+
+
+
+    private void getCoordinates() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location loc = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        if (loc == null) {
+            loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (loc == null) {
+                lon = 100;
+                lat = 100;
+            }
+        }
+        lon = loc.getLongitude();
+        lat = loc.getLatitude();
+    }
+
+    private void yelp() {
+        String consumerKey = "GH0hCC83JR1G-T_7T54jxw";
+        String consumerSecret = "Dw-cj6EtFAIRpu9pzSRjEuHEUNs";
+        String token = "VPvzcqEfLh07yrHaAzIARouynBWnDjxv";
+        String tokenSecret = "QYcm0Coq4XRfjLTSfyCv4Zlb38c";
+        YelpAPIFactory apiFactory = new YelpAPIFactory(consumerKey, consumerSecret, token, tokenSecret);
+        YelpAPI yelpAPI = apiFactory.createAPI();
+
+        Map<String, String> params = new HashMap<>();
+        // general params
+        params.put("term", term);
+        params.put("limit", numberOfResults);
+        params.put("category_filter", category_filter);
+
+        // build a coordinate object for the Yelp API to understand
+        CoordinateOptions coordinate = CoordinateOptions.builder()
+                .latitude(lat)
+                .longitude(lon).build();
+
+        // call request to API
+        Call<SearchResponse> call = yelpAPI.search(coordinate, params);
+
+        // setup for asynchronous request
+        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+
+            //TextView con = (TextView) findViewById(R.id.resultsView);
+            String con_test = "";
+
+            @Override
+            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+                ImageView img = (ImageView) findViewById(R.id.image);
+                ImageView img2 = (ImageView) findViewById(R.id.image2);
+                ImageView img3 = (ImageView) findViewById(R.id.image3);
+                ImageView img4 = (ImageView) findViewById(R.id.image4);
+                ImageView img5 = (ImageView) findViewById(R.id.image5);
+                ImageView img6 = (ImageView) findViewById(R.id.image6);
+                ImageView img7 = (ImageView) findViewById(R.id.image7);
+                ImageView img8 = (ImageView) findViewById(R.id.image8);
+
+
+                //res=response.body().toString();
+
+                SearchResponse searchResponse = response.body();
+                final ArrayList<Business> businesses = searchResponse.businesses();
+                // TextView text = (TextView) findViewById(R.id.resultView);
+                //String businessString= businesses.get(0).toString();
+
+                //text.setText(businessString);
+
+                //pass results to next activity
+                final String businessName = businesses.get(0).name();
+                String businessAddress;
+                try {
+                    businessAddress = businesses.get(0).location().address().get(0);
+                } catch (Exception e) {
+                    businessAddress = "no address available";
+                }
+
+                final String businessPhoneNumber = businesses.get(0).displayPhone();
+                double distance = businesses.get(0).distance();
+                final String city = businesses.get(0).location().city();
+                final String state = businesses.get(0).location().stateCode();
+                final String reviewSnippet = businesses.get(0).snippetText();
+                final String imageURL = businesses.get(0).imageUrl();
+                // convert meters to miles
+                distance = distance / 1609.34;
+
+
+                // text.setText(res);
+                for (int i = 0; i < Integer.parseInt(numberOfResults); i++)
+                {
+                    String address;
+                    try {
+                        address =businesses.get(i).location().address().get(i) ;
+                    }
+                    catch (Exception e){
+                        address = "no address available";
+                    }
+                    restaurant[i] = new Restaurant(businesses.get(i).name(), businesses.get(i).phone(),
+                            address, businesses.get(i).distance(), businesses.get(i).snippetText(),
+                            businesses.get(i).imageUrl(),businesses.get(i).location().city(),businesses.get(i).location().stateCode());
+                }
+                Picasso.with(getApplicationContext()).load(businesses.get(0).imageUrl()).resize(250, 250).centerInside().into(img);
+                Picasso.with(getApplicationContext()).load(restaurant[1].getImageURL()).resize(250,250).centerInside().into(img2);
+                Picasso.with(getApplicationContext()).load(restaurant[2].getImageURL()).resize(250,250).centerInside().into(img3);
+                Picasso.with(getApplicationContext()).load(restaurant[3].getImageURL()).resize(250,250).centerInside().into(img4);
+                Picasso.with(getApplicationContext()).load(restaurant[4].getImageURL()).resize(250,250).centerInside().into(img5);
+                Picasso.with(getApplicationContext()).load(restaurant[5].getImageURL()).resize(250,250).centerInside().into(img6);
+                Picasso.with(getApplicationContext()).load(restaurant[6].getImageURL()).resize(250,250).centerInside().into(img7);
+                Picasso.with(getApplicationContext()).load(restaurant[7].getImageURL()).resize(250,250).centerInside().into(img8);
+
+                //Picasso.with(getApplicationContext()).load(businesses.get(2).imageUrl()).resize(250,250).centerInside().into(img3);
+                //Picasso.with(getApplicationContext()).load(businesses.get(3).imageUrl()).resize(250,250).centerInside().into(img4);
+                //Picasso.with(getApplicationContext()).load(businesses.get(4).imageUrl()).resize(250,250).centerInside().into(img5);
+                //Picasso.with(getApplicationContext()).load(businesses.get(5).imageUrl()).resize(250,250).centerInside().into(img6);
+                //Picasso.with(getApplicationContext()).load(businesses.get(6).imageUrl()).resize(250,250).centerInside().into(img7);
+                //Picasso.with(getApplicationContext()).load(businesses.get(7).imageUrl()).resize(250,250).centerInside().into(img8);
+                final String finalBusinessAddress = businessAddress;
+                final double finalDistance = distance;
+
+                img.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        Intent toRestaurantDetails = new Intent(Restaurant_Results.this, RestaurantDetails.class);
+
+                        toRestaurantDetails.putExtra("name", businessName);
+                        toRestaurantDetails.putExtra("address", finalBusinessAddress);
+                        toRestaurantDetails.putExtra("phoneNumber", businessPhoneNumber);
+                        toRestaurantDetails.putExtra("imageURL", imageURL);
+                        toRestaurantDetails.putExtra("city", city);
+                        toRestaurantDetails.putExtra("state", state);
+                        toRestaurantDetails.putExtra("reviewSnippet", reviewSnippet);
+                        toRestaurantDetails.putExtra("distance", finalDistance);
+                        toRestaurantDetails.putExtra("term", term);
+                        startActivity(toRestaurantDetails);
+                        return false;
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(Call<SearchResponse> call, Throwable t) {
+                // HTTP error happened, do something to handle it.
+                TextView connectionTest = (TextView) findViewById(R.id.test);
+                con_test = "No conection";
+                connectionTest.setText(con_test);
+            }
+
+        };
+
+
+
+
+        // make the asynchronous request
+           call.enqueue(callback);
+    }
+    @Override
+    public void onBackPressed()
+    {
+           super.onBackPressed();
+          Intent intent = new Intent(this, MainActivity.class);
+          startActivity(intent);
+      }
+}
